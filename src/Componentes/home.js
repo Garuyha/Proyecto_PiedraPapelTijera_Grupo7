efd953beee8d7bca14dd659f7855292332c14e02
Carolina Maria de los Angeles Cortez <112413900+ChiaroCoure@users.noreply.github.com>
2022-09-21 19:35:36 -0300

    Update home.js

--- src/Componentes/home.js
+++ src/Componentes/home.js
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 /**Hola estoy intentando hacer esto que funcione asi evitamos romper el proyecto.
  */
 //Heyyy, me gustan los gatos
+=======
+//Hola gente que onda
+//holaaaaa
+>>>>>>> Stashed changes
