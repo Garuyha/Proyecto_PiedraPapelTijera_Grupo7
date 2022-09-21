e36a0dde7021a6a0e25d055fe275009d6d889b4d
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-09-21 20:03:53 -0300

    Revert "Chiaro coure"

--- src/Componentes/home.js
+++ src/Componentes/home.js
@@ -1,12 +1,2 @@
-<<<<<<< Updated upstream
 /**Hola estoy intentando hacer esto que funcione asi evitamos romper el proyecto.
  */
-//Heyyy, me gustan los gatos
-//no me acuerdo que comente, pero había comentado algo
-=======
-//Hola gente que onda
-//holaaaaa
->>>>>>> Stashed changes
-//Heyyy, me gustan los gatos
-//no me acuerdo que comente, pero había comentado algo
-//gay el que lea
