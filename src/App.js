45109b46eeb1f672cc7b28a047961334769c2046
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-09-24 19:48:12 -0300

    Cambios
    Inicio del desarrollo de trabajo de la hoja de juego

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App(){
+    return(
+        <div className="App">
+          <header className="App-header">
+          {/* <h1>Numero al azar: {numero}</h1> */}
+            <h1 className='Titulo'>Piedra, Papel o Tijera</h1>
+            <h2 className='jugador'>
+            JUGADOR                   IA
+            </h2>
+           <p>GANADOR</p>
+          <>
+          <ButtonGroup size="lg" className="mb-2">
+            <Button className='boton'><img  className='boton' alt="logo" /></Button>
+            <Button className='boton'><img  className='boton' alt="logo" /></Button>
+            <Button className='boton'><img  className='boton' alt="logo" /></Button>
+          </ButtonGroup>
+          </>
+           <p className="puntajes">puntajeJugador1:0    puntajeIA:0 </p> 
+        </header>
+        </div>
+        );
 }
-
-export default App;
