acdcd54c679dd4af142a7c8fb8b4d6073668bccb
HeNrYx94 <henryunju.fi@gmail.com>
2022-09-23 21:05:15 -0300

    Juego 1.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+// import {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Piedra from './img/Piedra.png';
+import Papel from './img/Papel.png';
+import Tijera from './img/Tijera.png';
+
+// {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+
 
 function App() {
+  // const [numero, setNumero] = useState(Math.floor(Math.random()* 10));
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* <h1>Numero al azar: {numero}</h1> */}
+        <h1 className='Titulo'>Piedra, Papel o Tijera</h1>
+        <h2 className='jugador'>
+        JUGADOR                   IA
+        </h2>
+       <p>GANADOR</p>
+      <>
+      <ButtonGroup size="lg" className="mb-2">
+        <Button className='boton'><img src={Piedra} className='boton' alt="logo" /></Button>
+        <Button className='boton'><img src={Papel} className='boton' alt="logo" /></Button>
+        <Button className='boton'><img src={Tijera} className='boton' alt="logo" /></Button>
+      </ButtonGroup>
+      </>
+       <p className="puntajes">puntajeJugador1:0    puntajeIA:0 </p>  
+  
+
+
+    
+
+
       </header>
     </div>
   );
---
+++ src/Componentes/juego.js
@@ -0,0 +1,26 @@
+import React, {Componentes} from "react";
+
+
+
+class Juego extends Componentes{
+
+    render(){
+     
+        return(
+            <React.Fragment>
+                
+             <ul id="final">
+            <li><strong>HOME</strong></li>
+            
+            </ul>
+            <div class="derechos"><p><strong>Copyright © 2022 Todos los derechos reservados</strong></p>
+            </div>
+        
+            <hr/>
+            </React.Fragment>
+        )
+    }
+    
+}
+
+export default Juego;
