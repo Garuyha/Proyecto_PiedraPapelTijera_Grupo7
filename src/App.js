1b400d30bb2809e77b2ee63a219ddf2a9286c7dc
HeNrYx94 <henryunju.fi@gmail.com>
2022-09-25 19:48:04 -0300

    cambios no importantes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import Piedra from './img/Piedra.png';
 import Papel from './img/Papel.png';
 import Tijera from './img/Tijera.png';
 
-// {/* <img src={logo} className="App-logo" alt="logo" /> */}
-
-
 
 function App() {
-  // const [numero, setNumero] = useState(Math.floor(Math.random()* 10));
-
 
   return (
     <div className="App">
       <header className="App-header">
-      {/* <h1>Numero al azar: {numero}</h1> */}
         <h1 className='Titulo'>Piedra, Papel o Tijera</h1>
         <h2 className='jugador'>
-        JUGADOR                   IA
+        IA
+        </h2>
+        <h2 className='jugador'>
+        JUGADOR
         </h2>
        <p>GANADOR</p>
       <>
@@ -30,16 +27,16 @@ function App() {
         <Button className='boton'><img src={Tijera} className='boton' alt="logo" /></Button>
       </ButtonGroup>
       </>
-       <p className="puntajes">puntajeJugador1:0    puntajeIA:0 </p>  
-  
-
-
-    
-
+       <p className="puntajes">puntajeJugador1:0    puntajeIA:0 </p> 
+      
 
       </header>
     </div>
   );
 }
+
+
+
+
 
 export default App;
--- src/Componentes/juego.js
+++ src/Componentes/juego.js
@@ -9,14 +9,6 @@ class Juego extends Componentes{
         return(
             <React.Fragment>
                 
-             <ul id="final">
-            <li><strong>HOME</strong></li>
-            
-            </ul>
-            <div class="derechos"><p><strong>Copyright © 2022 Todos los derechos reservados</strong></p>
-            </div>
-        
-            <hr/>
             </React.Fragment>
         )
     }
