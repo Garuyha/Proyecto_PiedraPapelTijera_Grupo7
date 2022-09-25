cf85ec505bd307466083a7bd0f24ad1da03103b3
Ezequiel Alejandro Garcia <112414829+Garuyha@users.noreply.github.com>
2022-09-25 17:43:24 -0300

    Cambios
    Últimos cambios y ajustes para la pagina juego, falta la implementación de las visuales correctas en el css.

--- src/Juego.js
+++ src/Juego.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Opcion from "./Opcion";
+import { Resultado } from "./Resultado";
 import './Juego.css';
 
 export function Juego(){
+    const[eleccionJugador,setEleccionJugador] = useState({});
+    const[eleccionMaquina, setEleccionMaquina] = useState({});
+
     const opciones =[
         {
             eleccion: "piedra",
@@ -18,10 +22,33 @@ export function Juego(){
         }
     ]
 
+    const elegirOpcion = (event) =>{
+        const jugador = opciones.find(e=>e.eleccion === event.target.textContent);
+        setEleccionJugador(jugador);
+        eleccionRival();
+    }
+
+    const eleccionRival = () =>{
+        const eleccion = opciones[Math.floor(Math.random()*opciones.length)];
+        setEleccionMaquina(eleccion);
+    }
+
     return(
         <div className = "Contenedor">
+            <Resultado jugador={eleccionJugador} maquina={eleccionMaquina}/>
+            <main>
+                <section>
+                    <div className="jugador">Jugador</div>
+                    <div className="eleccion">{eleccionJugador.eleccion}</div>
+                </section>
+                <section>
+                    <div className="maquina">Maquina</div>
+                    <div className="eleccion">{eleccionMaquina.eleccion}</div>
+                </section>
+            </main>
             <div className = "opciones">
-                {opciones.map((e, index)=><Opcion
+                {opciones.map((e,index)=><Opcion key={e=Math.random()} 
+                    elegir={elegirOpcion}
                     valor = {opciones[index]}/>)}
             </div>
         </div>
--- src/Opcion.js
+++ src/Opcion.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Opcion = (props)=>{
     return(
-        <div className="opcion">
+        <div className="opcion" onClick={props.elegir}>
             {props.valor.eleccion}
         </div>
     )
