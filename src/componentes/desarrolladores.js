d1a2b2af19071f4ef7afdb4e3753e3af76e9a1ab
Carolina Maria de los Angeles Cortez <112413900+ChiaroCoure@users.noreply.github.com>
2022-09-25 21:55:43 -0300

    Desarrolladores

--- src/componentes/desarrolladores.js
+++ src/componentes/desarrolladores.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Btn from "./Btn";
 import './css/style.css'
-import avatar from './img/Avatar.png';
+import Ivan from './img/Desarrolladores/Ivan.PNG'
+import Eze from './img/Desarrolladores/Eze.PNG'
+import Caro from './img/Desarrolladores/Caro.PNG'
+import Henry from './img/Desarrolladores/Henry.PNG'
+import Vale from './img/Desarrolladores/Vale.PNG'
 
 
  class Desarrolladores extends React.Component{
@@ -13,11 +17,12 @@ render (){
 
               <section className="tarjeta"> 
               <div className="contenedor">
-                 <img  src={avatar} width="150" alt=""/>
+                 <img  src={Ivan} alt="" className="imag"/>
                  <h3> <strong> Diaz Hector Ivan</strong></h3>
                  <h2> <strong> Programador</strong> </h2>
                  <p> DNI:37304786 </p>
                  <p> LU:37304786  </p>
+                 <p> Branch: IvanDiaz  </p>
                  </div>
               </section>
 
@@ -25,11 +30,12 @@ render (){
      
               <section className="tarjeta"> 
               <div className="contenedor">
-              <img  src={avatar} width="150" alt=""/>
+              <img  src={Eze} alt="" className="imag"/>
               <h3> <strong> Ezequiel Garcia</strong> </h3>
               <h2> <strong> Programador   </strong>  </h2>
               <p> DNI:37304786 </p>
               <p> LU:37304786  </p>
+              <p> Branch: Garcis  </p>
               </div>
                </section>
 
@@ -39,11 +45,12 @@ render (){
 
              <section className="tarjeta"> 
              <div className="contenedor">
-               < img  src={avatar} width="150" alt=""/>
+             <img  src={Caro} alt="" className="imag"/>
               <h3> <strong>  Carolina Cortez </strong> </h3>
               <h2> <strong> Programadora </strong>   </h2>
               <p> DNI:37304786 </p>
               <p> LU:37304786  </p>
+              <p> Branch: Charo  </p>
               </div>
             </section>
 
@@ -57,22 +64,24 @@ render (){
 
             <section className="tarjeta"> 
             <div className="contenedor">
-                < img  src={avatar} width="150" alt=""/>
-                <h3> <strong> Henrique Arias</strong> </h3>
+            <img  src={Henry} alt="" className="imag"/>
+                <h3> <strong> Enrique Arias</strong> </h3>
                 <h2> <strong> Programador  </strong> </h2>
                 <p> DNI:37304786 </p>
                 <p> LU:37304786  </p>
+                <p> Branch:HeNrYx94 </p>
                 </div>
               </section>
 
 
             <section className="tarjeta"> 
             <div className="contenedor">
-               < img  src={avatar} width="150" alt=""/>
+            <img  src={Vale} alt="" className="imag"/>
                <h3> <strong> Valeria Caballero</strong></h3>
                <h2> <strong> Programadora   </strong></h2>
                <p> DNI:37304786 </p>
                <p> LU:37304786  </p>
+               <p> Branch:ValeriaEmiliaCaballero </p>
                </div>
             </section>
 
